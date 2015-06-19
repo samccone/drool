19c249eb31087f1d490d263831402ef2ac61f1fe
Sam Saccone <samccone@gmail.com>
2015-06-18 22:03:34 -0400

    :tangerine:

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var options = new chrome.Options();
 options.addArguments('--js-flags=--expose-gc');
 options.setLoggingPrefs({performance: 'ALL'});
 options.setPerfLoggingPrefs({
-  'traceCategories': 'blink.console,disabled-by-default-devtools.timeline',
+  'traceCategories': 'blink.console,disabled-by-default-devtools.timeline'
   //Fix found here https://github.com/cabbiepete/browser-perf/commit/046f65f02db418c17ec2d59c43abcc0de642a60f
   // related to bug https://code.google.com/p/chromium/issues/detail?can=2&start=0&num=100&q=&colspec=ID%20Pri%20M%20Week%20ReleaseBlock%20Cr%20Status%20Owner%20Summary%20OS%20Modified&groupby=&sort=&id=474667
   //enableTimeline: true
