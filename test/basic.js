8866127a845f6ecc7680e98e5f274ac8ce847d3c
Sam Saccone <samccone@gmail.com>
2015-07-07 16:26:20 -0400

    :hammer:
    Set up initial interface

---
+++ test/basic.js
@@ -0,0 +1,26 @@
+var webdriver = require('selenium-webdriver');
+var leaky = require('../lib/');
+var path = require('path');
+var results = [];
+
+var driver = leaky.start();
+
+driver.get('file://' + path.join(__dirname, 'test.html'));
+
+leaky.getCounts(driver)
+.then(console.log.bind(console));
+
+driver.findElement(webdriver.By.css('#leak')).click();
+driver.findElement(webdriver.By.css('#leak')).click();
+driver.findElement(webdriver.By.css('#leak')).click();
+driver.findElement(webdriver.By.css('#leak')).click();
+
+leaky.getCounts(driver)
+.then(console.log.bind(console));
+
+driver.findElement(webdriver.By.css('#clean')).click();
+
+leaky.getCounts(driver)
+.then(console.log.bind(console));
+
+driver.quit();
