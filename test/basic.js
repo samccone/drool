dad70de11ff3da8717ab179364fd29362a89f554
Sam Saccone <samccone@gmail.com>
2015-07-07 17:49:28 -0400

    :volcano: :speedboat:
    Actually fail

--- test/basic.js
+++ test/basic.js
@@ -1,6 +1,7 @@
 var webdriver = require('selenium-webdriver');
 var leaky = require('../lib/');
 var path = require('path');
+var assert = require('assert');
 var results = [];
 
 var driver = leaky.start({
@@ -11,7 +12,7 @@ var driver = leaky.start({
 driver.get('file://' + path.join(__dirname, 'test.html'));
 
 leaky.getCounts(driver)
-.then(console.log.bind(console));
+.then(results.push.bind(results));
 
 driver.findElement(webdriver.By.css('#leak')).click();
 driver.findElement(webdriver.By.css('#leak')).click();
@@ -19,11 +20,15 @@ driver.findElement(webdriver.By.css('#leak')).click();
 driver.findElement(webdriver.By.css('#leak')).click();
 
 leaky.getCounts(driver)
-.then(console.log.bind(console));
+.then(function(data) {
+  assert.notEqual(results[0].nodes, data.nodes, "node count does not match");
+});
 
 driver.findElement(webdriver.By.css('#clean')).click();
 
 leaky.getCounts(driver)
-.then(console.log.bind(console));
+.then(function(data) {
+  assert.equal(results[0].nodes, data.nodes, "node count does not grow");
+});
 
 driver.quit();
